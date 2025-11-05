0f9f7d2841f5af4ff81161d807a72f44117132c5
ideil04 <moonideil04@gmail.com>
2025-11-05 18:45:09 +0900

    ✏️ chore: js 파일에서 사이트 이름 수정

--- styles/js/config.js
+++ styles/js/config.js
@@ -15,7 +15,7 @@ let charadex = {};
 /* Any preview links will still show Charadex's information
 /* ==================================================================== */
 charadex.site = {
-  title: "Charadex",
+  title: "Conneclife",
   url: "https://charadex-team.github.io/charadex-v1.0/",
   description: `A tool for organizing small ARPGs and species.`
 }
